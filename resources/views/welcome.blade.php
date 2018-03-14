491254b0b1f9ba34e5ede20b17af0551edeb1688
adedamolagig <adedamolagig@gmail.com>
2018-03-14 19:01:52 +0100

    commented out the animation test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 margin-bottom: 30px;
             }
         </style>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+        <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">  -->
     </head>
     <body>
         
