d2cb9d6001bbbd482b877a1d852ef534ce33df5c
adedamolagig <adedamolagig@gmail.com>
2020-06-21 13:30:18 +0100

    updated delete method with a reply policy

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -28,9 +28,10 @@ class RepliesController extends Controller
 
     public function destroy(Reply $reply)
     {
-        if($reply->user_id != auth()->id()){
-            return response([], 403);
-        }
+        // if($reply->user_id != auth()->id()){
+        //     return response([], 403);
+        // }
+        $this->authorize('update', $reply);
         $reply->delete();
 
         return back();
